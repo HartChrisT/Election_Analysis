0a7153b738fc524c24894f2a49f40d40cc38be9c
HartChrisT <92996865+HartChrisT@users.noreply.github.com>
2022-01-06 22:32:45 -0500

    updating PyPoll.py file

--- python_practice_2.py
+++ python_practice_2.py
@@ -1,3 +1,30 @@
-counties = ['Arapahoe','Denver','Jefferson']
-if counties[1] == 'Denver':
-        print(counties[1])
+'''
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, votes in counties_dict.items():
+        print(county+" county has ",str(votes)+" registered votes.")
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for i in range(len(voting_data)):
+        print(voting_data[i]['registered_voters'])
+
+for county_dict in voting_data:
+        for value in county_dict.values():
+                print(value)
+
+for county_dict in voting_data:
+        print(county_dict['registered_voters'])
+
+for county_dict in voting_data:
+        print(county_dict['county'])
+'''
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for i in range(len(voting_data)):
+        print(f'{voting_data[i]["county"]} county has {voting_data[i]["registered_voters"]} registered voters')
